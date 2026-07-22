bc325f929095c0073a70a16c90faa9a6b42acace
pie-trab <pietrotrabuio@gmail.com>
2026-07-22 16:00:06 +0200

    Multicast setup on tcl test file

--- DESERT_Framework/DESERT/samples/desert_samples/PHY/LumaXUV/test_uwlumaxuvmodem.tcl
+++ DESERT_Framework/DESERT/samples/desert_samples/PHY/LumaXUV/test_uwlumaxuvmodem.tcl
@@ -329,12 +329,19 @@ createNode
 # Put here all the commands required to connect nodes in the network (optional), namely, specify end to end connections, fill ARP tables, define routing settings
 
 # connections at the application level
-$app_ set destAddr_ [expr $opt(dest)]       ; # destAddr_ ID destinatario
-$app_ set destPort_ 1                       ; # 1 applicativo porta 1, porta interna packet destination port 2 bit di default
+# $app_ set destAddr_ [expr $opt(dest)]       ; # destAddr_ ID destinatario
+# $app_ set destPort_ 1                       ; # 1 applicativo porta 1, porta interna packet destination port 2 bit di default
 
+# broadcast address (255)
+$app_ set destAddr_ 255
 
-$routing_ addRoute $opt(dest) $opt(dest)    ; # utile per multihop, nexthop->destinatario // multihop altro script
-$mll_ addentry  $opt(dest) $opt(dest)
+# $routing_ addRoute $opt(dest) $opt(dest)    ; # utile per multihop, nexthop->destinatario // multihop altro script
+# $mll_ addentry  $opt(dest) $opt(dest)
+# Route destination 255 to next-hop 255
+$routing_ addRoute 255 255
+# Map IP 255 to MAC 255
+$mll_ addentry 255 255
+
 
 #####################
 # Start/Stop Timers #
